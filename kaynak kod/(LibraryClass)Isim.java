e1609b3c8208125cafb5b6f95be62f0a1020ad1a
BunyaminSanli <60830225+BunyaminSanli@users.noreply.github.com>
2020-04-11 16:47:22 +0300

    txt den rastgele isim çeken library class

---
+++ kaynak kod/(LibraryClass)Isim.java
@@ -0,0 +1,49 @@
+/*
+*
+* @author Bünyamin Şanlı
+* bunyaminsanli@ooutlook.com.tr
+* @since 11.04.2020
+* 
+*/
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author bunyamin
+ */
+public class Isim {
+    String satir = "";
+    String s = "";
+    String [] veri = new String[3000];
+    public String isimUret()
+    {
+        //dosya okuma
+    try{   
+         FileInputStream fStream = new FileInputStream("random_isimler.txt");
+         DataInputStream dStream = new DataInputStream(fStream);
+         BufferedReader bReader = new BufferedReader(new InputStreamReader(dStream));
+         
+         int i=0;
+         while((satir = bReader.readLine()) != null)
+         {
+            //null gürene kadar satırı okuyup Stringe atar onu da veri dizisine atar
+            veri[i] = satir;
+            i++;
+         }
+         dStream.close();
+         }
+         catch(Exception e)
+         {
+             System.out.println("hata" + e.getMessage());
+         }
+    
+    int a = (int)(System.nanoTime() % 3000);
+    s = veri[a];
+    return s;
+    }
+    
+}
