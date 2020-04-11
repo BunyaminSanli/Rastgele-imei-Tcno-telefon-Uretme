659708ddd682ecc4caa9c6e7176b1fb37c6d0885
BunyaminSanli <60830225+BunyaminSanli@users.noreply.github.com>
2020-04-11 16:48:39 +0300

    rastgele yas ureten library class

---
+++ kaynak kod/(LibraryClass)Yas.java
@@ -0,0 +1,23 @@
+/*
+*
+* @author Bünyamin Şanlı
+* bunyaminsanli@ooutlook.com.tr
+* @since 11.04.2020
+* 
+*/
+
+package rastgelekisiuret;
+
+/**
+ *
+ * @author bunyamin
+ */
+//random yaş ureten sınıf
+public class Yas {
+    int yas;
+    public int yasUret()
+    {
+        yas = (int)(System.nanoTime()% 101);
+        return yas;
+    }
+}
